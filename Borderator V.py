e5fe248ffe939c38f86b26919831be06296ae15b
Gautham Kumar <gauthamkumar017@gmail.com>
2021-04-20 10:48:24 +0100

    License and documentation

--- Borderator V.py
+++ Borderator V.py
@@ -28,7 +28,6 @@ print(f"This is Borderator {version}")
 def runMode():
     inputPath = fpEntry.get().replace('"', '')
 
-    # consider expanding?
     validFiletypes = ".jpg", ".jpeg",".png", ".tiff"
 
     if os.path.isdir(inputPath):
@@ -189,3 +188,18 @@ borderateButton.grid(column=0, row=12, columnspan=2, sticky='nesw')
 
 # MAINLOOP FOREVER
 window.mainloop()
+
+"""
+Borderator
+Copyright (C) 2021 Gautham Kumar
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+"""
